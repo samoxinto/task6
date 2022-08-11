8b45b8bd128917fda972a51f5607eadabcf319f6
Денис Степаненко <denis.stepanenko@hotmail.com>
2022-08-11 09:43:39 +0300

    Add project files.

---
+++ Task6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Результат");
+
+            if (a % 2 == 0)
+                Console.WriteLine("Да");
+            else
+                Console.WriteLine("Нет");
+
+            Console.ReadLine();
+        }
+    }
+}
